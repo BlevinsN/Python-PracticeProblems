bf97f091c6586b4e1fc0889735e4202f359cae20
Nate Blevins <48926188+BlevinsN@users.noreply.github.com>
2020-07-28 18:17:51 -0500

    Create FindPercentage.py

---
+++ FindPercentage.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    student_marks = {}
+    for _ in range(n):
+        line = raw_input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = raw_input()
+    x = student_marks[query_name]
+    Average = 0.00
+    for i in x:
+        Average += i
+    print(format(Average / len(x), '.2f'))
